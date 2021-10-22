f3e5fb27a2f8ae4c758322615497341cf4c7e20d
Hanwool Lee <hanwool26@gmail.com>
2021-10-22 17:04:01 +0900

    Update socket_client.py

--- src/socket_client.py
+++ src/socket_client.py
@@ -1,4 +1,6 @@
 import socket
+import threading
+import json
 
 class Socket_Client():
     def __init__(self, address, port):
@@ -6,10 +8,28 @@ class Socket_Client():
         self.port = port
 
         self.client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.threads = [threading.Thread(target=self.__recv, daemon=False), ]
+
+
+    def __recv(self):
+        while True:
+            data = self.client_sock.recv(1024)
+            if data != b'':
+                print(data.decode())
 
     def connection(self):
         self.client_sock.connect((self.server_addr, self.port))
+        for t in self.threads:
+            t.start()
+
 
 if __name__ == '__main__':
-    
+    client_sock = Socket_Client('10.157.8.216', 9999)
+    client_sock.connection()
+    TEST_JSON = {'coin_name': '리플',
+                 'balance': 100000,
+                 'interval': 6,
+                 'repeat': False}
+    data = json.dumps(TEST_JSON)
 
+    client_sock.client_sock.sendall(data.encode())
