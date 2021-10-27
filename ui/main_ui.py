e0b8f9a01506307f9f2455831b9a2f43fe3086b2
Hanwool Lee <hanwool26@gmail.com>
2021-10-28 08:51:16 +0900

    ui update

--- ui/main_ui.py
+++ ui/main_ui.py
@@ -14,7 +14,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(742, 673)
+        MainWindow.resize(911, 673)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
